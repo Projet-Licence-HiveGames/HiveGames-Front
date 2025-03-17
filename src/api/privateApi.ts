27719aa5f13c0676047c93cd469883a3b11a0ce8
vgtom4 <tom.vieillard.grelet@gmail.com>
2025-03-17 20:54:38 +0100

    Add `GameSessionProvider` and `GameSessionContext`

--- src/api/privateApi.ts
+++ src/api/privateApi.ts
@@ -1,3 +1,7 @@
+import { useContext, useMemo } from "react";
+import { toast } from "react-hot-toast";
+import { AuthContext } from "../context/AuthProvider";
+
 export const privateApi = async <T>(
     endpoint: string,
     method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET',
@@ -27,3 +31,98 @@ export const privateApi = async <T>(
 
     return await response.json() as T;
 };
+
+export type APIOptions = { withAuth?: boolean } & RequestInit;
+export type RequestInitWithHeaders = RequestInit & {
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  headers?: { [key: string]: any };
+};
+
+export const useFetch = () => {
+    const { logout } = useContext(AuthContext);
+  
+    const wrapFetch = ({ headers, ...requestOptions }: RequestInit) => {
+      return async (
+        path: string,
+        // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+        body?: any,
+        { withAuth, ...options }: APIOptions = { withAuth: true },
+      ) => {
+        const buildOptions: RequestInitWithHeaders = {
+          ...options,
+          ...requestOptions,
+        };
+        buildOptions.headers = { ...buildOptions.headers, ...headers };
+        if (body) {
+          if (body instanceof FormData) {
+            buildOptions.body = body;
+          } else {
+            buildOptions.body = JSON.stringify(body);
+            buildOptions.headers = {
+              ...buildOptions.headers,
+              ...{
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+              },
+              credentials: 'include'
+            };
+          }
+        }
+        const url = `${import.meta.env.HIVEGAMES_BACKEND_API}${path}`;
+  
+        if (withAuth ?? true) {
+          // const token =
+          //   access_token == null || tokenHasExpired(access_token)
+          //     ? await refresh()
+          //     : access_token;
+  
+          // if (token == null) {
+          //   return new Response(null, { status: 401 });
+          // }
+          // buildOptions.headers.Authorization = `Bearer ${token}`;
+  
+          return fetch(url, buildOptions).then((res) =>
+            handleResponse(res, async () => {
+              // if (buildOptions?.headers?.Authorization) {
+              //   buildOptions.headers.Authorization = `Bearer ${await refresh()}`;
+              // }
+              return fetch(url, buildOptions).then(handleResponse);
+            }),
+          );
+        }
+  
+        return fetch(url, buildOptions).then(handleResponse);
+      };
+    };
+  
+    const handleResponse = async (
+      response: Response,
+      retry?: () => Promise<Response>,
+    ) => {
+      if (!response.ok) {
+        if (response.status == 401) {
+          if (retry) {
+            return await retry();
+          }
+          logout();
+        } else if (response.status >= 500) {
+          toast.error(
+            'Service momentanément indisponible. Veuillez réessayer plus tard',
+          );
+        }
+        throw new Error(await response.text());
+      }
+      return response;
+    };
+  
+    return useMemo(
+      () => ({
+        get: wrapFetch({ method: 'GET' }),
+        post: wrapFetch({ method: 'POST' }),
+        put: wrapFetch({ method: 'PUT' }),
+        patch: wrapFetch({ method: 'PATCH' }),
+        delete: wrapFetch({ method: 'DELETE' }),
+      }),
+      [], // eslint-disable-line  react-hooks/exhaustive-deps
+    );
+  };
