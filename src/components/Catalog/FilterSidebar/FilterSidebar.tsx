01945aca1c80f944fc0962a2bfd5831ad017363f
vgtom4 <tom.vieillard.grelet@gmail.com>
2025-03-18 21:13:33 +0100

    Improve `Dropdown`

--- src/components/Catalog/FilterSidebar/FilterSidebar.tsx
+++ src/components/Catalog/FilterSidebar/FilterSidebar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import classNames from 'classnames';
 
 import './FilterSidebar.css';
@@ -6,6 +6,8 @@ import { MaterialSymbol } from 'react-material-symbols';
 import Select from '@mui/joy/Select';
 import Option from '@mui/joy/Option';
 import { Box, Chip, Slider } from '@mui/joy';
+import Dropdown, { DropdownOption } from './Dropdown/Dropdown';
+import { GameCategory } from '../../../types/Game';
 
 export interface GameFilter {
   search: string;
@@ -25,6 +27,32 @@ interface FilterSidebarProps {
 }
 
 const FilterSidebar: FC<FilterSidebarProps> = ({ className, filters, setFilters }) => {
+  const [selectedCategories, setSelectedCategories] = useState<number[]>([]);
+  const [categories, setCategories] = useState<GameCategory[]>([
+    {id: 1, label: "rpg"},
+    {id: 2, label: "sandbox"},
+    {id: 3, label: "fps"},
+    {id: 4, label: "pvp"},
+    {id: 5, label: "pve"},
+    {id: 6, label: "shooter"},
+    {id: 7, label: "rogue_like"},
+  ]);
+
+  const priceLabel = (() => {
+    const min = filters.prices.min === 0 ? 'Gratuit' : `${filters.prices.min} €`
+    const max = filters.prices.max === 101 ? '+100 €' : `${filters.prices.max} €`
+    if (filters.prices.min == filters.prices.max) {
+      if (filters.prices.min === 0) {
+        return 'Gratuit';
+      } else if (filters.prices.min === 101) {
+        return '+100 €';
+      } else {
+        return `${filters.prices.min} €`;
+      }
+    } else {
+      return `Entre ${min} et ${max}`;
+    }
+  })();
   return (
     <div className={classNames('filter-sidebar-container', className)}>
       <div className='filter-sidebar-header'>
@@ -42,42 +70,20 @@ const FilterSidebar: FC<FilterSidebarProps> = ({ className, filters, setFilters 
         valueLabelFormat={(value) => value > 0 ? `${value > 100 ? "+100" : value} €` : "gratuit"}
         getAriaValueText={(value) => value > 0 ? `${value > 100 ? "+100" : value} €` : "gratuit"}
         max={101}
-        marks={[
-          {
-            value: 0,
-            label: 'Gratuit',
-          },
-          {
-            value: 101,
-            label: '+100 €',
-          },
-        ]}
+        // marks={[
+        //   {
+        //     value: 0,
+        //     label: filters.prices.min == 0 ? "Gratuit" : filters.prices.min == 101 ? "+100 €" : `${filters.prices.min} €`,
+        //   },
+        //   {
+        //     value: 101,
+        //     label: filters.prices.max == 0 ? "Gratuit" : filters.prices.max == 101 ? "+100 €" : `${filters.prices.max} €`,
+        //   },
+        // ]}
       />
-      <Select
-        multiple
-        defaultValue={['dog', 'cat']}
-        renderValue={(selected) => (
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '0.25rem' }}>
-            {selected.map((selectedOption, index) => (
-              <Chip key={index} variant="soft" color="primary">
-                {selectedOption.label}
-              </Chip>
-            ))}
-          </Box>
-        )}
-        slotProps={{
-          listbox: {
-            sx: {
-              width: '100%',
-            },
-          },
-        }}
-      >
-        <Option value="dog">Dog</Option>
-        <Option value="cat">Cat</Option>
-        <Option value="fish">Fish</Option>
-        <Option value="bird">Bird</Option>
-      </Select>
+      <span>{priceLabel}</span>
+
+      <Dropdown title='Categories' options={categories.map((category) => ({ label: category.label, value: category.id } as DropdownOption))} selected={selectedCategories} setSelected={(selected) => setSelectedCategories(selected as number[])}/>
     </div>
   );
 };
