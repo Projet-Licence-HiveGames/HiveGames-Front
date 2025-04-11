2d36de3e9502045026a843d5a9a4ac3192491d88
A-Fevre <adrien.fevre.pro@gmail.com>
2025-04-11 10:35:10 +0200

    changed background progress bar, added skeleton loading

--- src/pages/Catalogue/Catalogue.tsx
+++ src/pages/Catalogue/Catalogue.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Catalogue.css";
 import GameCard from "../../components/GameCard/GameCard.tsx";
 import FilterSidebar, {
@@ -10,32 +10,24 @@ import GameCardSkeleton from "../../components/GameCard/GameCardSkeleton.tsx";
 
 export const Catalogue: React.FC = () => {
   const fetchAPI = useFetch();
-  const [filters, setFilters] = React.useState<GameFilter>({
+  const [isLoading, setIsLoading] = useState(true);
+  const [filters, setFilters] = useState<GameFilter>({
     search: "",
     categories: [],
     languages: [],
     features: [],
     prices: { min: 0, max: 101 },
   });
-  const [gameList, setGameList] = React.useState<Game[]>([
-    // { id: 1, name: "Game 1", price: 100, oldPrice: 200 },
-    // { id: 2, name: "Game 2", price: 200, oldPrice: 0 },
-    // { id: 3, name: "Game 3", price: 300, oldPrice: 350 },
-    // { id: 4, name: "Game 4", price: 400, oldPrice: 420 },
-    // { id: 5, name: "Game 5", price: 500 },
-    // { id: 6, name: "Game 6", price: 600 },
-    // { id: 7, name: "Game 7", price: 700 },
-    // { id: 8, name: "Game 8", price: 800 },
-    // { id: 9, name: "Game 9", price: 900 },
-    // { id: 10, name: "Game 10", price: 1000 },
-  ]);
+  const [gameList, setGameList] = useState<Game[]>([]);
 
   const fetchGames = async () => {
     try {
+      setIsLoading(true);
       await fetchAPI.post<Game[]>("/games", filters).then(setGameList);
-    }
-    catch (error) {
+    } catch (error) {
       console.error("Erreur lors de la récupération des jeux :", error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -54,11 +46,15 @@ export const Catalogue: React.FC = () => {
   return (
     <div className="catalog-container">
       <div className="catalog-container-cards">
-        <Suspense fallback={<GameCardSkeleton />}>
-          {gameList.map((game, index) => (
+        {isLoading ? (
+          Array(20).fill(0).map((_, index) => (
+            <GameCardSkeleton key={index} />
+          ))
+        ) : (
+          gameList.map((game, index) => (
             <GameCard key={index} game={game} />
-          ))}
-        </Suspense>
+          ))
+        )}
       </div>
       <FilterSidebar filters={filters} setFilters={setFilters} />
     </div>
