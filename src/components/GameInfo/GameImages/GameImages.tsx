d794256248bd75a6497b32bf186a2fad3c665f06
A-Fevre <adrien.fevre.pro@gmail.com>
2025-04-18 18:34:57 +0200

    used image from back

--- src/components/GameInfo/GameImages/GameImages.tsx
+++ src/components/GameInfo/GameImages/GameImages.tsx
@@ -1,30 +1,38 @@
 import React from "react";
 import Flicking from "@egjs/react-flicking";
+import { GameImage } from "../../../types/Game";
+import image from "@assets/images/image_49.png";
 
 import "@egjs/react-flicking/dist/flicking.css";
 import "./GameImages.css";
 
-interface ImageType {
-  file_name: string;
-  alt: string;
-}
-
 interface GameImagesProps {
-  images: ImageType[];
+  images?: GameImage[];
 }
 
 export const GameImages: React.FC<GameImagesProps> = ({ images }) => {
+  const slides = images?.filter(i => i.file_name.startsWith('slide') && i.file_url)
+  const _images = slides?.length ?
+    slides :
+    [images?.find(i => i.file_name.startsWith('thumbnail') && i.file_url) ?? {
+      id: 0,
+      file_name: "thumbnail",
+      alt: "Image 1",
+      file_url: image
+    } as GameImage];
+
   return (
     <Flicking
       className="game-images"
       renderOnlyVisible={true}
       horizontal={true}
     >
-      {images.map((image, index) => (
+      {_images.map((image, index) => (
         <div className="flicking-panel" key={index}>
-          <img src={image.file_name} alt={image.alt} />
+          <img src={image?.file_url} alt={image?.alt} />
         </div>
-      ))}
+      )
+      )}
     </Flicking>
   );
 };
