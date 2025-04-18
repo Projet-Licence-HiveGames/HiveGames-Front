03acbd98c786ce45a146481984f4e2991314e62d
A-Fevre <adrien.fevre.pro@gmail.com>
2025-04-18 18:34:57 +0200

    used image from back

--- src/components/GameInfo/GameDescription/GameDescription.tsx
+++ src/components/GameInfo/GameDescription/GameDescription.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-
+import { GameImage } from "../../../types/Game";
+import image from "@assets/images/image_49.png";
 import "./GameDescription.css";
-
-interface ImageType {
-  file_name: string;
-  alt?: string;
-}
 
 interface GameDescriptionProps {
   description: string;
-  images: ImageType[];
+  thumbnail?: GameImage;
 }
 
 export const GameDescription: React.FC<GameDescriptionProps> = ({
   description,
-  images,
+  thumbnail,
 }) => {
   return (
     <div className="game-description-container">
       <div className="game-images">
-        <img src={images[0]?.file_name} alt={images[0]?.alt} />
+        <img src={thumbnail?.file_url ?? image} alt={thumbnail?.alt} />
       </div>
       <div className="game-description">
         <p>{description}</p>
--- src/pages/Game/GameInfo.tsx
+++ src/pages/Game/GameInfo.tsx
@@ -34,11 +34,9 @@ export const GameInfo: React.FC = () => {
       <div className="game-info-container">
         {!isMobile && (
           <div className="game-info-container-left">
-            <img src={gameData?.images?.[0]?.file_name} alt={gameData?.name} />
+            {/* <img src={gameData?.images?.find(i=>i.file_name.startsWith('slide'))} alt={gameData?.name} /> */}
             <GameImages
-              images={
-                gameData?.images ? gameData.images.map((image) => image) : []
-              }
+              images={gameData?.images}
             />
           </div>
         )}
@@ -51,9 +49,7 @@ export const GameInfo: React.FC = () => {
           {!isMobile ? (
             <GameDescription
               description={gameData?.description || ""}
-              images={
-                gameData?.images ? gameData.images.map((image) => image) : []
-              }
+              thumbnail={gameData?.images?.find(i => i.file_name.startsWith('thumbnail'))}
             />
           ) : (
             <img src={gameData?.images?.[0]?.file_name} alt={gameData?.name} />
