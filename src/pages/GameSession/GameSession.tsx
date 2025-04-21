9d30b79b23d5ddf7893c9eeca89bd0eea792a618
vgtom4 <tom.vieillard.grelet@gmail.com>
2025-04-21 20:30:03 +0200

    chore: update Cookie Clicker url

--- src/pages/GameSession/GameSession.tsx
+++ src/pages/GameSession/GameSession.tsx
@@ -63,7 +63,7 @@ export const GameSession: FC<GameSessionProps> = ({
         <div className="game-session-overlay">
           {/* <iframe src='https://hivegames-cloudgaming.com/session/128a8b2e-b647-4d1e-8792-70e8154df585' title='Game title' width={'100%'} height={'100%'} /> */}
           <iframe
-            src="https://projet-licence-hivegames.github.io/Cookie-Clicker-Source-Code/"
+            src="https://projet-licence-hivegames.github.io/cookie-clicker/"
             title="Cookie Clicker"
             width={"100%"}
             height={"100%"}
