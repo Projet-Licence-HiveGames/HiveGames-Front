403c49bd1c096378e491040cf22ac6ef6fe759a2
A-Fevre <adrien.fevre.pro@gmail.com>
2025-05-21 23:42:52 +0200

    Removed useEffect

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -3,15 +3,21 @@ import { Navigate, useLocation } from "react-router-dom";
 
 import { useAuth } from "../context/AuthProvider";
 
+import { Loader } from "./Loader/Loader.tsx";
+
 interface ProtectedRouteProps {
   children: React.ReactNode;
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { isAuthenticated } = useAuth();
+  const { user, loading } = useAuth();
   const location = useLocation();
 
-  if (!isAuthenticated) {
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
