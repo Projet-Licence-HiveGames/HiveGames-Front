17b5618d8b330d498d0281c9984b11df42143402
A-Fevre <adrien.fevre.pro@gmail.com>
2025-05-22 11:37:52 +0200

    feat: add GameUnderPrice component to display games under €10 and update layout styles
    # Conflicts:
    #	src/components/GameCard/GameCard.css

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { FC, useContext } from "react";
 
 import { GameOfTheWeek } from "../../components/Home/GameOfTheWeek/GameOfTheWeek.tsx";
+import { GameUnderPrice } from "../../components/Home/GameUnderPrice/GameUnderPrice.tsx";
 import { TopCategories } from "../../components/Home/TopCategories/TopCategories.tsx";
 import { GameSessionContext } from "../../context/GameSessionProvider";
 
@@ -13,8 +14,11 @@ export const Home: FC = () => {
       <button onClick={() => startGameSession(1, "Cookie Clicker")}>
         Start
       </button>
-      <GameOfTheWeek />
-      <TopCategories />
+      <div className={"home-body"}>
+        <GameOfTheWeek />
+        <TopCategories />
+        <GameUnderPrice />
+      </div>
     </div>
   );
 };
