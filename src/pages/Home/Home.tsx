5863f0937935f49abb3db00a3a37390aa5f0aa9c
vgtom4 <tom.vieillard.grelet@gmail.com>
2025-06-20 14:52:45 +0200

    refactor: remove unused import from Home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,8 @@
-import { FC, useContext } from "react";
+import { FC } from "react";
 
 import { GameOfTheWeek } from "../../components/Home/GameOfTheWeek/GameOfTheWeek.tsx";
 import { GameUnderPrice } from "../../components/Home/GameUnderPrice/GameUnderPrice.tsx";
 import { TopCategories } from "../../components/Home/TopCategories/TopCategories.tsx";
-import { GameSessionContext } from "../../context/GameSessionProvider";
 
 import "./Home.css";
 
