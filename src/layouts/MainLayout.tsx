e90456c14b240cdad22ec3af4a262eb9ca089ce5
vgtom4 <tom.vieillard.grelet@gmail.com>
2025-06-20 14:04:03 +0200

    refactor: replace React.FC with FC for MainLayout component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { FC } from "react";
 import { Outlet } from "react-router-dom";
 
 import MainHeader from "./MainHeader/MainHeader.tsx";
@@ -6,7 +6,7 @@ import Sidebar from "./Sidebar/Sidebar.tsx";
 
 import "./MainLayout.css";
 
-export const MainLayout: React.FC = () => {
+export const MainLayout: FC = () => {
   return (
     <div className="app-layout">
       <Sidebar />
