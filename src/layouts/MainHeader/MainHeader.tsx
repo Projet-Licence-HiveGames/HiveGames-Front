b688ed3d9d809e4861f1107e0296dfbf79619108
vgtom4 <tom.vieillard.grelet@gmail.com>
2025-06-20 12:54:20 +0200

    refactor: simplify Sidebar component and enhance toggle functionality

--- src/layouts/MainHeader/MainHeader.tsx
+++ src/layouts/MainHeader/MainHeader.tsx
@@ -1,20 +1,13 @@
-import React from "react";
-import { MenuRounded, Search } from "@mui/icons-material";
+import { FC } from "react";
+import { Search } from "@mui/icons-material";
 
 import { GroupMenu } from "../../components/GroupMenu/GroupMenu.tsx";
 
 import "./MainHeader.css";
 
-interface MainHeaderProps {
-  onSidebarToggle: () => void;
-}
-
-const MainHeader: React.FC<MainHeaderProps> = ({ onSidebarToggle }) => {
+const MainHeader: FC = () => {
   return (
     <div className="main-header">
-      <button className="sidebar-toggle" onClick={onSidebarToggle}>
-        <MenuRounded />
-      </button>
       <div className="search-bar">
         <Search className="search-icon" />
         <input
