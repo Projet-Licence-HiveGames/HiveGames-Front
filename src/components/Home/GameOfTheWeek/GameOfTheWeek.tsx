4c3766a6cb3b7e6a5bdb9b3a429771bbb94e9706
A-Fevre <adrien.fevre.pro@gmail.com>
2025-06-22 13:28:34 +0200

    feat: enhance game data fetching by integrating user-specific game collection details and improving loading logic

--- src/components/Home/GameOfTheWeek/GameOfTheWeek.tsx
+++ src/components/Home/GameOfTheWeek/GameOfTheWeek.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 
 import { useFetch } from "../../../api/privateApi.ts";
 import { useAuth } from "../../../context/AuthProvider.tsx";
-import { GameBaseType } from "../../../types/Game.ts";
+import { findGameCollection, GameBaseType } from "../../../types/Game.ts";
 import { HomeSkeleton } from "../../Skeleton/Home/HomeSkeleton.tsx";
 import { TLabel } from "../../ui/TranslationLabel/TLabel.tsx";
 
@@ -12,20 +12,30 @@ import "./GameOfTheWeek.css";
 
 export const GameOfTheWeek: React.FC = () => {
   const [gameData, setGameData] = useState<GameBaseType[]>([]);
-  const [loading, setLoading] = useState(true);
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, user } = useAuth();
   const fetchAPI = useFetch();
   const fetchData = useCallback(async () => {
-    await fetchAPI
-      .get<GameBaseType[]>("/games/week")
-      .then(setGameData)
-      .catch((error) => console.error("Error fetching data:", error))
-      .finally(() => setLoading(false));
-  }, []);
+    try {
+      const data = await fetchAPI.get<GameBaseType[]>("/games/week");
+      setGameData(
+        data.map((game) => {
+          const collection = findGameCollection(game.id, user);
+          return {
+            ...game,
+            is_wished: collection?.is_wished || false,
+            is_owned: collection?.is_owned || false,
+            be_notified: collection?.be_notified || false,
+          };
+        }),
+      );
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }, [fetchAPI]);
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [user]);
 
   return (
     <div className={"game-of-the-week-container"}>
@@ -34,7 +44,7 @@ export const GameOfTheWeek: React.FC = () => {
           <TLabel baliseType={"h2"} label="games_of_the_week" />
         </div>
         <div className={"game-of-the-week-image"}>
-          {loading ? (
+          {gameData.length === 0 ? (
             <HomeSkeleton />
           ) : (
             <GameImageHP games={gameData} isAuthenticated={isAuthenticated} />
--- src/pages/Catalog/Catalog.tsx
+++ src/pages/Catalog/Catalog.tsx
@@ -47,7 +47,7 @@ export const Catalog: FC = () => {
       )
       .catch(() => toast.error("Erreur lors de la récupération des jeux"));
     setIsLoading(false);
-  }, [filters, fetchAPI, user?.game_collections]);
+  }, [filters, fetchAPI]);
 
   useEffect(() => {
     const params = Object.fromEntries(searchParams.entries());
@@ -108,7 +108,7 @@ export const Catalog: FC = () => {
     }, 500);
 
     return () => clearTimeout(timeoutId);
-  }, [filters]);
+  }, [filters, user]);
 
   const renderedGames = useMemo(() => {
     return isLoading
